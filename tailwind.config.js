b4afb2b9b1b2f3b7dfa0e8abe8023fdd9dc75ce7
Cizz22 <auliacisatra14@gmail.com>
2021-11-16 20:44:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: {
+      content: [
+        './resources/views/**/*.blade.php',
+        './storage/framework/views/*.php',
+      ],
+      option: {
+        safelist: [
+            'sm:max-w-2xl'
+        ]
+      }
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+],
+}
