0cddacc45affad2cd85aa89a10f1445a35e78636
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-21 00:38:57 +0700

    add landing route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('landing.about');
+    return view('landing.about', [
+        "title" => "About"
+    ]);
 });
 
