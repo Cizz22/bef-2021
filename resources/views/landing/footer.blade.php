fb8ded1f333d81e48b5a127ed9d480786acc0f97
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-21 00:39:40 +0700

    add footer landing blade

---
+++ resources/views/landing/footer.blade.php
@@ -0,0 +1,55 @@
+<footer>
+    <div class="pesan">
+      <h5 class="text-center">Apa yang ingin kamu sampaikan?</h5>
+      <form action="#">
+        <div class="mb-3 input-group">
+          <input type="text" class="form-control" placeholder="Ketik disini">
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        </div>
+      </form>
+    </div>
+    <div class="container py-5">
+      <nav>
+        <div class="row">
+          <div class="col d-flex justify-content-center align-items-center">
+            <a href="#" class="nav-link">Sharing & Motivation</a>
+          </div>
+          <div class="col d-flex justify-content-center align-items-center">
+            <a href="#" class="nav-link">Simulasi UTBK</a>
+          </div>
+          <div class="col d-flex justify-content-center align-items-center">
+            <a href="#" class="nav-link">Expo Kampus</a>
+          </div>
+          <div class="col d-flex justify-content-center align-items-center">
+            <a href="#" class="nav-link">Blog</a>
+          </div>
+          <div class="col-sm-4 col-12">
+            <div class="container-fluid">
+              <div class="row">
+                <h4>Temukan Kami di Sosial Media!</h4>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <a href="#" class="social-link">
+                    <i class="fab fa-instagram" aria-hidden="true"></i>
+                  </a>
+                  <a href="#" class="social-link">
+                    <i class="fab fa-twitter" aria-hidden="true"></i>
+                  </a>
+                  <a href="#" class="social-link">
+                    <i class="fab fa-youtube" aria-hidden="true"></i>
+                  </a>
+                  <a href="#" class="social-link">
+                    <i class="fab fa-tiktok"></i>
+                  </a>
+                  <a href="#" class="social-link">
+                    <i class="fab fa-line"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </footer>
