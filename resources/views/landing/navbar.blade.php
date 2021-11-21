3bc711a282c36424e0f3724f624fa3ab03efed1e
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-21 23:43:17 +0700

    navbar logo link

--- resources/views/landing/navbar.blade.php
+++ resources/views/landing/navbar.blade.php
@@ -1,26 +1,26 @@
 <nav class="px-1 navbar navbar-expand-lg navbar-dark px-sm-5 fixed-top" style="background: rgba(12, 38, 76, 1);"">
     <div class="flex-row-reverse container-fluid">
-<a class="navbar-brand" href="#">
-  <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="logo-bef">
-</a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="/">About</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Event</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Kontak</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Blog</a>
-    </li>
-  </ul>
-</div>
+        <a class="navbar-brand" href="/">
+        <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="logo-bef">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">About</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Event</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Kontak</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+            </li>
+        </ul>
+    </div>
 </div>
 </nav>
