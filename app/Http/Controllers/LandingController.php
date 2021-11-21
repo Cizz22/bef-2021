9511eba26d0bc06c62510f2a2d0ef3182bc996a1
Cizz22 <auliacisatra14@gmail.com>
2021-11-21 16:36:35 +0700

    add migration, authentication for admin, & Landing page controller

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('landing.about', [
+            "title" => "About"
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Untuk simpan data pendaftar
+
+
+    }
+}
