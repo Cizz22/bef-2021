3148b90ecacf2c7fdee97538cea3cd7f967340ab
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-26 18:42:24 +0700

    add form & sponsor section

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -4,18 +4,18 @@
   <header class="d-flex flex-column justify-content-center align-items-center">
     <div class="container">
       <div class="row">
-          <div class="col">
-              <h1 class="text-center">Brebes Education Fair 2022</h1>
-          </div>
+        <div class="col">
+          <h1 class="text-center">Brebes Education Fair 2022</h1>
+        </div>
       </div>
     </div>
     <div class="container">
       <div class="row">
-          <div class="text-center col">
-              <a href="#main">
-                <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
-              </a>
-          </div>
+        <div class="text-center col">
+          <a href="#main" class="hover">
+            <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
+          </a>
+        </div>
       </div>
     </div>
   </header>
@@ -28,7 +28,7 @@
           <h1 class="section-title">Kenalan Yuk!</h1>
         </div>
         <div class="row">
-          <img src="{{ asset('img/svg/logo-bef.svg') }}" class="border rounded-circle" alt="logo-bef">
+          <img src="img/svg/logo-bef.svg" class="border rounded-circle" alt="logo-bef">
         </div>
         <div class="row">
           <p class="text-center text-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque enim eum
@@ -51,27 +51,27 @@
         </div>
         <div class="row">
           <div class="text-center col">
-            <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="">
+            <img src="img/svg/logo-bef.svg" alt="">
             <h4>Ghifary Muhammad</h4>
             <p>Sistem Informasi ITS</p>
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="">
+            <img src="img/svg/logo-bef.svg" alt="">
             <h4>Ghifary Muhammad</h4>
             <p>Sistem Informasi ITS</p>
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="">
+            <img src="img/svg/logo-bef.svg" alt="">
             <h4>Ghifary Muhammad</h4>
             <p>Sistem Informasi ITS</p>
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="">
+            <img src="img/svg/logo-bef.svg" alt="">
             <h4>Ghifary Muhammad</h4>
             <p>Sistem Informasi ITS</p>
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/svg/logo-bef.svg') }}" alt="">
+            <img src="img/svg/logo-bef.svg" alt="">
             <h4>Ghifary Muhammad</h4>
             <p>Sistem Informasi ITS</p>
           </div>
@@ -82,75 +82,75 @@
 
     <section class="expo-campus">
       <div class="container">
-        <div class="row"  data-aos="fade-up" data-aos-duration="1500">
+        <div class="row" data-aos="fade-up" data-aos-duration="1500">
           <h1 class="text-center section-title">Expo Campus</h1>
         </div>
         <div class="row" data-aos="fade-up" data-aos-duration="1500">
           <div class="text-center col">
-            <img src="{{ asset('img/png/ui.png') }}" alt="ui">
+            <img src="img/png/ui.png" alt="ui">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/itb.png') }}" alt="itb">
+            <img src="img/png/itb.png" alt="itb">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/ugm.png') }}" alt="ugm">
+            <img src="img/png/ugm.png" alt="ugm">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/unair.png') }}" alt="unair">
+            <img src="img/png/unair.png" alt="unair">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/its.png') }}" alt="its">
-          </div>
-        </div>
-        <div class="row" data-aos="fade-up" data-aos-duration="1500">
-          <div class="text-center col">
-            <img src="{{ asset('img/png/ipb.png') }}" alt="ipb">
-          </div>
-          <div class="text-center col">
-            <img src="{{ asset('img/png/unbraw.png') }}" alt="unbraw">
-          </div>
-          <div class="text-center col">
-            <img src="{{ asset('img/png/undip.png') }}" alt="undip">
-          </div>
-          <div class="text-center col">
-            <img src="{{ asset('img/png/uns.png') }}" alt="uns">
-          </div>
-          <div class="text-center col">
-            <img src="{{ asset('img/png/unpad.png') }}" alt="unpad">
+            <img src="img/png/its.png" alt="its">
           </div>
         </div>
         <div class="row" data-aos="fade-up" data-aos-duration="1500">
           <div class="text-center col">
-            <img src="{{ asset('img/png/unnes.png') }}" alt="unnes">
+            <img src="img/png/ipb.png" alt="ipb">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/unsoed.png') }}" alt="unsoed">
+            <img src="img/png/unbraw.png" alt="unbraw">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/utel.png') }}" alt="utel">
+            <img src="img/png/undip.png" alt="undip">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/utidar.png') }}" alt="utidar">
+            <img src="img/png/uns.png" alt="uns">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/umsurakarta.png') }}" alt="umsurakarta">
+            <img src="img/png/unpad.png" alt="unpad">
           </div>
         </div>
         <div class="row" data-aos="fade-up" data-aos-duration="1500">
           <div class="text-center col">
-            <img src="{{ asset('img/png/uii.png') }}" alt="uii">
+            <img src="img/png/unnes.png" alt="unnes">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/unissula.png') }}" alt="unissula">
+            <img src="img/png/unsoed.png" alt="unsoed">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/uin.png') }}" alt="uin">
+            <img src="img/png/utel.png" alt="utel">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/udinus.png') }}" alt="udinus">
+            <img src="img/png/utidar.png" alt="utidar">
           </div>
           <div class="text-center col">
-            <img src="{{ asset('img/png/umyogyakarta.png') }}" alt="umyogyakarta">
+            <img src="img/png/umsurakarta.png" alt="umsurakarta">
+          </div>
+        </div>
+        <div class="row" data-aos="fade-up" data-aos-duration="1500">
+          <div class="text-center col">
+            <img src="img/png/uii.png" alt="uii">
+          </div>
+          <div class="text-center col">
+            <img src="img/png/unissula.png" alt="unissula">
+          </div>
+          <div class="text-center col">
+            <img src="img/png/uin.png" alt="uin">
+          </div>
+          <div class="text-center col">
+            <img src="img/png/udinus.png" alt="udinus">
+          </div>
+          <div class="text-center col">
+            <img src="img/png/umyogyakarta.png" alt="umyogyakarta">
           </div>
         </div>
       </div>
@@ -173,6 +173,43 @@
         </div>
         <div class="row">
           <a href="#" class="main-btn">Kepoin Lebih Lanjut!</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="form-daftar">
+      <div class="container">
+        <div class="my-5 row" data-aos="fade-up" data-aos-duration="1500">
+          <div class="col">
+            <h2 class="text-center">Ayo buruan daftar sekarang juga dan raih PTN <br> impianmu....</h2>
+          </div>
+        </div>
+        <div class="my-5 row"  data-aos="fade-up" data-aos-duration="1500">
+          <div class="text-center col">
+            <a href="#" target="_blank" id="btn-daftar">GAS</a>
+          </div>
+        </div>
+        <div class="row"></div>
+      </div>
+    </section>
+
+    <section class="sponsor">
+      <div class="container">
+        <div class="my-5 row">
+          <div class="col">
+            <h1 class="text-center section-title">Media Partner & Sponsorship</h1>
+          </div>
+        </div>
+        <div class="py-5 my-5 text-center row">
+          <div class="col">
+              <img src="img/svg/logo-bef.svg" alt="">
+          </div>
+          <div class="col">
+              <img src="img/svg/logo-bef.svg" alt="">
+          </div>
+          <div class="col">
+              <img src="img/svg/logo-bef.svg" alt="">
+          </div>
         </div>
       </div>
     </section>
