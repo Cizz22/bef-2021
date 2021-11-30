87ef84f217495521f9619ee4f99447ab0d35ddfb
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-30 10:39:35 +0700

    add pesan-mobile

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -9,12 +9,17 @@
       </form>
     </div>
     <div class="container pt-5 mt-5">
-      <div class="row">
-
-      </div>
-      <div class="pesan-mobile">
-
-      </div>
+        <div class="row d-lg-none d-block">
+            <div class="pesan-mobile">
+              <h5 class="pb-4 text-center">Apa yang ingin kamu sampaikan?</h5>
+              <form action="#">
+                <div class="mb-3 input-group">
+                  <input type="text" class="form-control" placeholder="Ketik disini">
+                  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Kirim</button>
+                </div>
+              </form>
+            </div>
+        </div>
       <nav>
         <div class="row">
           <div class="col-lg d-flex align-self-stretch justify-content-center align-items-center">
