76e1ae00df56aa77bae5ee3919812f8f063b748a
Cizz22 <auliacisatra14@gmail.com>
2021-12-26 08:35:38 +0700

    add user admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,10 +15,40 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name'      => 'Admin Bef 2021',
-            'email'     => 'admin@bef.com',
-            'password'  => Hash::make('password1234'),
-        ]);
+        DB::table('users')->insert(
+            [
+            'name'      => 'Admin Bef 1',
+            'email'     => 'admin@bef1.com',
+            'password'  => Hash::make('adminbef2021nomer1'),
+            ],
+        );
+        DB::table('users')->insert(
+            [
+                'name'      => 'Admin Bef 2',
+                'email'     => 'admin@bef2.com',
+                'password'  => Hash::make('adminbef2021nomer2'),
+            ],
+        );
+        DB::table('users')->insert(
+            [
+                'name'      => 'Admin Bef 3',
+                'email'     => 'admin@bef3.com',
+                'password'  => Hash::make('adminbef2021nomer3'),
+            ],
+        );
+        DB::table('users')->insert(
+            [
+                'name'      => 'Admin Bef 4',
+                'email'     => 'admin@bef4.com',
+                'password'  => Hash::make('adminbef2021nomer3'),
+            ],
+        );
+        DB::table('users')->insert(
+            [
+                'name'      => 'Admin Bef 5',
+                'email'     => 'admin@bef5.com',
+                'password'  => Hash::make('adminbef2021nomer5'),
+            ],
+        );   
     }
 }
