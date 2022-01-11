1b665e7166d5a16ca0b47cc8762b1576fe225c2b
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2022-01-11 15:09:29 +0700

    move navbar & footer to partials folder

--- resources/views/landing/main.blade.php
+++ resources/views/landing/main.blade.php
@@ -35,13 +35,13 @@
         <i class="fa fa-arrow-up"></i>
     </div>
 
-    @include('landing.navbar')
+    @include('partials.navbar')
 
     <div>
         @yield('content')
     </div>
 
-    @include('landing.footer')
+    @include('partials.footer')
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
