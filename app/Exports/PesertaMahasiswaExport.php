a2a19d7d171ae6525435ebd97e749120f0c353c0
Cizz22 <auliacisatra14@gmail.com>
2022-02-13 12:38:53 +0700

    fix bug

--- app/Exports/PesertaMahasiswaExport.php
+++ app/Exports/PesertaMahasiswaExport.php
@@ -4,7 +4,6 @@ namespace App\Exports;
 
 use App\Models\Peserta;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
@@ -16,7 +15,7 @@ class PesertaMahasiswaExport implements FromQuery, WithMapping, WithHeadings
     public function query()
     {
         return Peserta::query()->where([
-            ['jenjang', '=', 'Mahasiwa'],
+            ['jenjang', '=', 'SMA'],
             ['payment_verif_status', '=','Terverifikasi']
         ]);
     }
